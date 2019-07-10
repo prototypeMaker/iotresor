0762e33347241ebff15a1ddfe010a85a288828d1
Kevin Kamto <kamtokevin@gmail.com>
2019-07-10 08:29:50 -0400

    feat: added logo to background

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 //Express setup
 const Express = require('express');
 const app = new Express();
-
+const path = require('path');
 
 
 var logger = (res, req, next) => {
@@ -15,15 +15,16 @@ module.exports = {
 
     init() {
         app.use(logger); //Logs pings
-        app.use(Express.static('./public'));
         console.log("Finished initializing")
     },
-
+    
     start(port) {
         //Server side Rendering
-        app.use("/", (req, res, next) => {
+        // app.use("../public", Express.static(__dirname + "."));
+        app.get("/", (req, res, next) => {
             //Serves empty html file
-            res.sendFile(path.join(__dirname, './public', 'index.html'))
+            // res.sendFile("/public/index.html")
+            res.sendFile(path.join(__dirname, '~/', 'index.html'))
         })
         app.listen(port, () => {console.log("Serving index.html")})
     }
